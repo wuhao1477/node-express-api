9f04a4c423eb368cbdc7f4672ea595d5f3e495c7
wuhao1477 <1477681049@qq.com>
2022-03-02 13:50:16 +0800

    更新了一下。因腾讯云函数不支持type：“module”的写法即import引入，故放弃云函数适配。

--- api.js
+++ api.js
@@ -1,16 +1,12 @@
-import request from "request"
-var cookie
+import request from "request";
+var cookie;
 
-// module.exports={
-//   main
-// }
-//
 export async function main(kuaidi_number) {
   let token = await getToken(kuaidi_number); //token可以多次使用的，但这里就不写多次使用了避免特殊情况发生
-  console.log("获取的token为："+token, "要查询的订单号为："+kuaidi_number);
+  console.log("获取的token为：" + token, "要查询的订单号为：" + kuaidi_number);
   let data = await kuaidi(token, kuaidi_number);
   console.log(data);
-  return data
+  return data;
 }
 //获取token和cookie的值
 export function getToken(kuaidi_number) {
@@ -23,7 +19,7 @@ export function getToken(kuaidi_number) {
       },
     };
     request.get(opts, (_e, b, d) => {
-      cookie = b.headers["set-cookie"].toString() //获取传回来的cookie值
+      cookie = b.headers["set-cookie"].toString(); //获取传回来的cookie值
       let checkUrl = d.substring(d.indexOf("checkUrl")); //截取字符串获取tokenv2的值
       checkUrl = checkUrl.substring(
         checkUrl.indexOf("tokenV2=") + 8,
@@ -35,20 +31,23 @@ export function getToken(kuaidi_number) {
 }
 
 //请求快递数据
-export function kuaidi(token,kuaidi_number) {
-  return new Promise((resolve)=>{
+export function kuaidi(token, kuaidi_number) {
+  return new Promise((resolve) => {
     let opts = {
-      url:"https://express.baidu.com/express/api/express?tokenV2="+token+"&nu="+kuaidi_number,
-      headers:{
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
-        "Connection":"keep-alive",
-        "Cookie": cookie
+      url:
+        "https://express.baidu.com/express/api/express?tokenV2=" +
+        token +
+        "&nu=" +
+        kuaidi_number,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
+        Connection: "keep-alive",
+        Cookie: cookie,
       },
-    }
-    request.get(opts,(_e,_d,b)=>{
-      resolve(JSON.parse(b))
-    })
-
-    
-  })
+    };
+    request.get(opts, (_e, _d, b) => {
+      resolve(JSON.parse(b));
+    });
+  });
 }
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import { main } from "./api.js"
-import express from "express"
-const app = express()
+import { main } from "./api.js";
+import express from "express";
+const app = express();
 
-
-app.listen(80)
-app.get("/",async(req,res)=>{
-    let order_number = req.query.order_number
-    let data = await main(order_number)
-    res.json(data)
-})
+app.listen(80);
+app.get("/", async (req, res) => {
+  let order_number = req.query.order_number;
+  let data = await main(order_number);
+  res.json(data);
+});
